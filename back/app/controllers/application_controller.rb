37361b6fa78cdeca8578bce8597a671a66bf98ea
ikurasussi <kaikai>
2025-09-25 16:03:08 +0900

    feature/add-user_api

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# 認可
+# リクエストヘッダーのトークンを常に検証する
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  private
+
+  def authonticate_user!
+    token = request.headers["Authorization"]&.spilit(" ")&.last
+    payload = FirebaseIdToken::Signature.verify(token)
+    return render json: { error: "Unauthorized" }, status: :unauthorized unless payload
+
+    @current_user = User.find_by(firebase_uid: payload["user_id"])
+    return render json: { error: "User not found" }, status: :unauthorized unless @current_user
+  end
 end
---
+++ back/config/initializers/firebase_id_token.rb
@@ -0,0 +1,3 @@
+FirebaseIdToken.configure do |config|
+  config.project_ids = ["your_firebase_project_id"] # FirebaseプロジェクIDD
+end
