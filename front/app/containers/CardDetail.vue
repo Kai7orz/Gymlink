a0e20e32d362a76e672a3daac0d360508f34ffc4
Kai7orz <kai7orz@gmail.com>
2025-10-30 11:47:50 +0900

    戻りボタン押下時のバグ修正

--- front/app/containers/CardDetail.vue
+++ front/app/containers/CardDetail.vue
@@ -1,62 +1,70 @@
 <script setup lang="ts">
-  import { useAuthStore } from '~/stores/auth';
-  const  route = useRoute();
-  const props = defineProps<{
-    id: number,
-  }>();
+import { useAuthStore } from "~/stores/auth";
+import { useDetailStore } from "~/stores/detailStore";
+const props = defineProps<{
+  id: number;
+  isOwner: boolean;
+}>();
 
-    
-  const auth = useAuthStore()
-  const TOKEN = auth.idToken
-  const data = ref(false)
-  const record_id = props.id
-  
-  data.value = await $fetch(`/api/likes/${record_id}`,{
-      method: 'GET',
-      headers: {
-                    'Authorization': 'Bearer ' + TOKEN,
-                    'Content-Type': 'application/json'  
-      }
-  })
-  
-  const toGood = async (id:number) =>{
-    try{
-        await $fetch("/api/likes",
-            {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'Bearer ' + TOKEN,
-                    'Content-Type': 'application/json'
-                },
-                body: {
-                    record_id: id
-                },
-            }
-        )
-    } catch(e){
-        console.log("likes post error: ",e)
-    }
-  }
+const auth = useAuthStore();
+const detailStore = useDetailStore();
+const TOKEN = auth.idToken;
+const data = ref(false);
+const record_id = props.id;
 
-  const toDelete = async (id:number) => {
-    const record_id = id
-    try{
-      await $fetch(`/api/likes/${record_id}`,{
-        method: "DELETE",
+data.value = await $fetch(`/api/likes/${record_id}`, {
+  method: "GET",
+  headers: {
+    "Authorization": "Bearer " + TOKEN,
+    "Content-Type": "application/json",
+  },
+});
+
+const toGood = async (id: number) => {
+  try {
+    await $fetch("/api/likes",
+      {
+        method: "POST",
         headers: {
-                    'Authorization': 'Bearer ' + TOKEN,
-                    'Content-Type': 'application/json'
-                  },
-      })
-    } catch(e){
-      console.log("delete error: ",e)
-    }
+          "Authorization": "Bearer " + TOKEN,
+          "Content-Type": "application/json",
+        },
+        body: {
+          record_id: id,
+        },
+      },
+    );
   }
+  catch (e) {
+    console.log("likes post error: ", e);
+  }
+};
+
+const toBack = () => {
+  if (detailStore.detailIsOwner) navigateTo({ name: "home" });
+  else navigateTo({ name: "share" });
+};
+
+const toDelete = async (id: number) => {
+  const record_id = id;
+  try {
+    await $fetch(`/api/likes/${record_id}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": "Bearer " + TOKEN,
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  catch (e) {
+    console.log("delete error: ", e);
+  }
+};
 
 </script>
 
 <template>
         <div>
-          <ui-card-detail :id="props.id" :liked="data.liked" @good="toGood" @delete="toDelete"/>
+          <ui-card-detail :id="props.id" :liked="data.liked" @good="toGood" @delete="toDelete" @back="toBack" />
         </div>
 </template>
--- front/app/stores/detailStore.ts
+++ front/app/stores/detailStore.ts
@@ -1,27 +1,29 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-export const useDetailStore = defineStore('detail',
-    ()=>{
-        const detailId = ref(0);
-        const detailUserId = ref(0);
-        const detailName = ref('')
-        const detailPresignedImage = ref('');
-        const detailTime = ref(0);
-        const detailDate = ref('');
-        const detailComment = ref('');
-        const detailLikesCount = ref(0);
-        
-        const setDetail = (id:number,user_id:number,name:string,presigned_image:string,clean_up_time:number,clean_up_date:string,comment:string,likesCount:number) => {
-            detailId.value = id;
-            detailUserId.value = user_id
-            detailName.value = name;
-            detailPresignedImage.value = presigned_image;
-            detailTime.value = clean_up_time;
-            detailDate.value = clean_up_date;
-            detailComment.value = comment;
-            detailLikesCount.value = likesCount;
-        }
-        
-        return { detailId,detailUserId,detailName,detailPresignedImage,detailTime,detailDate,detailComment,detailLikesCount,setDetail }
-        },
+export const useDetailStore = defineStore("detail",
+  () => {
+    const detailId = ref(0);
+    const detailUserId = ref(0);
+    const detailName = ref("");
+    const detailPresignedImage = ref("");
+    const detailTime = ref(0);
+    const detailDate = ref("");
+    const detailComment = ref("");
+    const detailLikesCount = ref(0);
+    const detailIsOwner = ref(false);
+    // isOwner は詳細画面前の画面が home なのか share かの判定に利用し，戻るボタン押下時の戻り先を変更する
+    const setDetail = (id: number, user_id: number, name: string, presigned_image: string, clean_up_time: number, clean_up_date: string, comment: string, likesCount: number, isOwner: boolean) => {
+      detailId.value = id;
+      detailUserId.value = user_id;
+      detailName.value = name;
+      detailPresignedImage.value = presigned_image;
+      detailTime.value = clean_up_time;
+      detailDate.value = clean_up_date;
+      detailComment.value = comment;
+      detailLikesCount.value = likesCount;
+      detailIsOwner.value = isOwner;
+    };
+
+    return { detailId, detailUserId, detailName, detailPresignedImage, detailTime, detailDate, detailComment, detailLikesCount, detailIsOwner, setDetail };
+  },
 );
