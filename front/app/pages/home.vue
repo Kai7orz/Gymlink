192b6080a54631aa184f35ce8e4b6c5c553ebd65
Kai7orz <kai7orz@gmail.com>
2025-10-31 20:29:25 +0900

    upload ボタンのコンポーネント化

--- front/app/pages/home.vue
+++ front/app/pages/home.vue
@@ -1,12 +1,23 @@
 <script lang="ts" setup>
-    import CardList from '~/containers/CardList.vue';
-    import AddCard from '~/containers/AddCard.vue';
-    const isOwner=ref(true)
+import UploadBtn from "~/containers/UploadBtn.vue";
+import CardList from "~/containers/CardList.vue";
+const isOwner = ref(true);
 </script>
 
 <template>
-    <v-container class="p-100 min-h-screen">
-        <add-card />
-        <card-list :isOwner="isOwner" />
+    <v-container  class="p-100 min-h-screen" >
+        <upload-btn />
+        <card-list :is-owner="isOwner" />
     </v-container>
 </template>
+
+<style>
+  .upload-pos {
+    display: flex;
+    justify-content: center;
+    margin: 20px;
+    @media (max-width:400px) {
+      margin:8px 0px;
+    }
+  }
+</style>
