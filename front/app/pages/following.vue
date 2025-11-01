4e8fd310c5a03c51344082114e763233c7d0de80
Kai7orz <kai7orz@gmail.com>
2025-11-01 18:40:53 +0900

    フォロー一覧のユーザー名表示

--- front/app/pages/following.vue
+++ front/app/pages/following.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-    console.log("following 画面への遷移")
+import FollowingList from "~/containers/FollowingList.vue";
+const pageLabel = "Following Users";
 </script>
 
 <template>
-
-
+<div class="text-center">
+  <h1 class="text-4xl m-10">{{ pageLabel }}</h1>
+  <following-list />
+</div>
 </template>
